7249aa86dc2d740a4b21efc9743ca3962bb1ea5d
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-03-23 10:43:22 +0300

    Add cross origin

--- src/main/java/com/puzzle/server/controllers/TimeController.java
+++ src/main/java/com/puzzle/server/controllers/TimeController.java
@@ -3,6 +3,7 @@ package com.puzzle.server.controllers;
 import com.puzzle.server.dto.Time;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import static org.springframework.http.HttpStatus.OK;
@@ -11,6 +12,7 @@ import static org.springframework.http.HttpStatus.OK;
 public class TimeController {
 
     @GetMapping("concert-start")
+    @CrossOrigin(origins = "http://localhost:8100")
     public ResponseEntity<Time> getConcertStartTime() {
         return new ResponseEntity<>(new Time("12:00:00"), OK);
     }
