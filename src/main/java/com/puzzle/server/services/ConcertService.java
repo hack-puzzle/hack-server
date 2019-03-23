d45ff955f48fd93991aa9173faf1efdb78c0978d
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-03-23 20:12:28 +0300

    Create two concert

--- src/main/java/com/puzzle/server/services/ConcertService.java
+++ src/main/java/com/puzzle/server/services/ConcertService.java
@@ -3,7 +3,6 @@ package com.puzzle.server.services;
 import com.puzzle.server.dto.ArtistInfo;
 import com.puzzle.server.dto.ConcertInfo;
 import com.puzzle.server.dto.ConcertUpdateInfo;
-import com.puzzle.server.dto.Time;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +24,25 @@ public class ConcertService {
         ConcertInfo concertInfo = new ConcertInfo();
         ArtistInfo artistInfo = new ArtistInfo();
         artistInfo.setName("Radio Tapok");
+        artistInfo.setSiteUrl("http://radiotapok.ru");
+        artistInfo.setPhotoUrl("assets/artists/radiotapok/tapok-icon.png");
         concertInfo.setArtistInfo(artistInfo);
         ConcertUpdateInfo concertUpdateInfo = new ConcertUpdateInfo();
-        concertUpdateInfo.setStartTime(timeFormatService.format("12.11.2012 11:12:00"));
+        concertUpdateInfo.setStartTime(timeFormatService.format("24.03.2019 19:00:00"));
         concertInfo.setConcertUpdateInfo(concertUpdateInfo);
         concertInfoMap.put("radiotapok", concertInfo);
+
+        // Team
+        concertInfo = new ConcertInfo();
+        artistInfo = new ArtistInfo();
+        artistInfo.setName("Team");
+        artistInfo.setSiteUrl("http://ifmo.ru");
+        artistInfo.setPhotoUrl("assets/artists/radiotapok/tapok-icon.png");
+        concertInfo.setArtistInfo(artistInfo);
+        concertUpdateInfo = new ConcertUpdateInfo();
+        concertUpdateInfo.setStartTime(timeFormatService.format("25.03.2019 20:12:00"));
+        concertInfo.setConcertUpdateInfo(concertUpdateInfo);
+        concertInfoMap.put("team", concertInfo);
     }
 
     public ConcertInfo getConcertInfo(String concertName) {
