10bbded5e54020107e650ce3e17be2ff0d37d7aa
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-03-24 09:48:00 +0300

    Add Song details

--- src/main/java/com/puzzle/server/dto/Song.java
+++ src/main/java/com/puzzle/server/dto/Song.java
@@ -2,11 +2,23 @@ package com.puzzle.server.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonSerialize
 @AllArgsConstructor
+@Getter
+@Setter
 public class Song {
 
     private String name;
+
+    public Song(String name) {
+        this.name = name;
+    }
+
+    private String photoUrl;
+    private String text;
+
 
 }
